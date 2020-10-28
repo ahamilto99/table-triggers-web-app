b0d2556b8b2ec52ad1c559037a237608b3c671cd
ahamilto99 <ajhamilton15@hotmail.com>
2020-10-28 12:15:08 -0400

    initial commit

---
+++ src/main/java/com/algonquin/cst8276/groupproject/triggerswebapp/OralceTableTriggersWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.algonquin.cst8276.groupproject.triggerswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OralceTableTriggersWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OralceTableTriggersWebAppApplication.class, args);
+	}
+
+}
